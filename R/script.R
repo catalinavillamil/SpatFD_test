8a99ca72db33b05cc2d12b5ccc0081e1845c584c
Catalina Villamil <acvillamils@unal.edu.co>
2020-10-08 15:59:04 -0500

    update

--- R/script.R
+++ R/script.R
@@ -3,7 +3,8 @@
 #------------------------------------------------------------------------
 
 library(tidyr)
-
+library(data.table)
+library(tidyverse)
 #------------------------------------------------------------------------
 #            PAQUETES NECESARIOS (Paquete)
 #------------------------------------------------------------------------
@@ -14,11 +15,6 @@ library(sp)
 library(gridExtra)
 library(GenSA)
 library(gtools)
-
-#------------------------------------------------------------------------
-#            PAQUETES NECESARIOS (Paquete)
-#------------------------------------------------------------------------
-
 library(ggplot2)
 
 #------------------------------------------------------------------------
@@ -37,41 +33,50 @@ namedi=function(X){
 #              DATOS
 #------------------------------------------------------------------------
 
-cont2017=read.csv("~/Docs/Estadistica/Trabajo de grado/Datos Arreglados/cont2017.csv")
+# cont2017=read.csv("~/Docs/Estadistica/Trabajo de grado/Datos Arreglados/cont2017.csv")
+# 
+# ##PM10
+# PM1017=cont2017[cont2017$id_parameter=="PM10",]
+# a=rep(1:8760,each=24)
+# PM1017[,1]=a
+# PM1017=spread(PM1017,id_station,value)
+# PM1017=namedi(PM1017)
+# PM1017=PM1017[,-c(19,25)]
+# PM1017=PM1017[,-c(11,14,17)]
+# PM1017=PM1017[,-7]
+# MPM1017=as.matrix(PM1017[,-c(1:3)])
+# MPM1017=MPM1017[-c(1:2299,2601:8760),]
+# datos=read.table("C:/Users/Catalina Villamil/Downloads/Mexico/Mexico/coordenadasmexico.txt",head=T,dec=",",row.names=1)
+# coordenadas=datos[c(1,3,4,5,8,11,16,17,18,24,26,31,34,37,38,39,42,43),c(1,2)]
+# #MPM1017=MPM1017[,-c(1,2,6,7,8,10,13,16,17,18)]
+# #coordenadas=coordenadas[-c(1,2,6,7,8,10,13,16,17,18),]
+# MPM1017=MPM1017[,-c(1,6,7)]
+# coordenadas=coordenadas[-c(1,6,7),]
+# coordenadas1=coordenadas
+# 
+# #NO2
+# NO217=cont2017[cont2017$id_parameter=="NO2",]
+# a2=rep(1:8760,each=29)
+# NO217[,1]=a2
+# NO217=spread(NO217,id_station,value)
+# NO217=namedi(NO217)
+# NO217=NO217[,-c(9,17)]
+# NO217=NO217[,-c(6,7,8,14,15,17,19,21,24,25,27,28)]
+# MNO217=as.matrix(NO217[,-c(1:3)])
+# MNO217=MNO217[-c(1:2299,2601:8760),]
+# MNO217=MNO217[,c(1,2,4,6,7,8,11,13,14,15)]
+# coordenadas2=datos[c(1,3,10,11,12,16,17,24,27,30,34,32,39,42,43),c(1,2)]
+# coordenadas2=coordenadas2[c(1,2,4,6,7,8,11,13,14,15),]
 
-##PM10
-PM1017=cont2017[cont2017$id_parameter=="PM10",]
-a=rep(1:8760,each=24)
-PM1017[,1]=a
-PM1017=spread(PM1017,id_station,value)
-PM1017=namedi(PM1017)
-PM1017=PM1017[,-c(19,25)]
-PM1017=PM1017[,-c(11,14,17)]
-PM1017=PM1017[,-7]
-MPM1017=as.matrix(PM1017[,-c(1:3)])
-MPM1017=MPM1017[-c(1:2299,2601:8760),]
-datos=read.table("C:/Users/Catalina Villamil/Downloads/Mexico/Mexico/coordenadasmexico.txt",head=T,dec=",",row.names=1)
-coordenadas=datos[c(1,3,4,5,8,11,16,17,18,24,26,31,34,37,38,39,42,43),c(1,2)]
-#MPM1017=MPM1017[,-c(1,2,6,7,8,10,13,16,17,18)]
-#coordenadas=coordenadas[-c(1,2,6,7,8,10,13,16,17,18),]
-MPM1017=MPM1017[,-c(1,6,7)]
-coordenadas=coordenadas[-c(1,6,7),]
-coordenadas1=coordenadas
-
-#NO2
-NO217=cont2017[cont2017$id_parameter=="NO2",]
-a2=rep(1:8760,each=29)
-NO217[,1]=a2
-NO217=spread(NO217,id_station,value)
-NO217=namedi(NO217)
-NO217=NO217[,-c(9,17)]
-NO217=NO217[,-c(6,7,8,14,15,17,19,21,24,25,27,28)]
-MNO217=as.matrix(NO217[,-c(1:3)])
-MNO217=MNO217[-c(1:2299,2601:8760),]
-MNO217=MNO217[,c(1,2,4,6,7,8,11,13,14,15)]
-coordenadas2=datos[c(1,3,10,11,12,16,17,24,27,30,34,32,39,42,43),c(1,2)]
-coordenadas2=coordenadas2[c(1,2,4,6,7,8,11,13,14,15),]
-
+MPM1017=fread("/home/usuario/Documentos/GitHub/SpatFD/data/MPM1017.csv") %>% 
+     column_to_rownames(var="V1")
+MNO217=fread("/home/usuario/Documentos/GitHub/SpatFD/data/MNO217.csv")%>% 
+     column_to_rownames(var="V1")
+coordenadas=fread("/home/usuario/Documentos/GitHub/SpatFD/data/coordenadas.csv")%>% 
+     column_to_rownames(var="V1")
+coordenadas2=fread("/home/usuario/Documentos/GitHub/SpatFD/data/coordenadas2.csv")%>% 
+     column_to_rownames(var="V1")
+# datos=read.table("C:/Users/Catalina Villamil/Downloads/Mexico/Mexico/coordenadasmexico.txt",head=T,dec=",",row.names=1)
 
 #------------------------------------------------------------------------#
 #            EJEMPLO
